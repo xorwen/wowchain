82c21ed53be3f8c51e01c985e708f37a4986f48b
Ondrej <ondrej.vesely@kiwi.com>
2018-06-23 15:44:04 +0200

    Add test endpoint

---
+++ app/api.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/ping')
+def chatbot_callback():
+    a = int(request.args.get('a', 0))
+    return jsonify({"pong": a})
