3df0c333f491aa5634746b7b0704b20c9e66ecca
me <pavel@pavelkral.eu>
2018-07-17 08:44:59 +0200

    format

--- app/api.py
+++ app/api.py
@@ -11,8 +11,8 @@ import time
 import pickle
 from flask_cors import cross_origin
 
-
 import sys
+
 sys.path.append('..')
 
 from blockchain.create_contract.create import CreateContract
@@ -30,6 +30,7 @@ blocks = {
     'get_documents': '5b30d9c5e4b08507221cc383'
 }
 
+
 def store_to_blockchain(token, name_a, name_b):
     contract_id = token
     out = f'./assets/{contract_id}-'
@@ -41,8 +42,9 @@ def store_to_blockchain(token, name_a, name_b):
     ]
     subprocess.Popen(command, cwd='..')
 
+
 def get_random_string(size=6, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 def broadcast(user_id, block_name, attributes=None):
@@ -68,10 +70,11 @@ def async_broadcast(user_id, message, block):
 
 @app.route('/api/test_broadcast')
 def chatbot_callback():
-    user_id = request.args.get('user_id','')
+    user_id = request.args.get('user_id', '')
     print(f"Broadcasting test mesage to {user_id}")
     async_broadcast(user_id, "test broadcast message")
     return jsonify({"ok": 1})
+
 
 @app.route('/api/test_marrige')
 def test_marrige():
@@ -82,7 +85,6 @@ def test_marrige():
 @app.route('/api/contract_state/<contract_id>')
 @cross_origin()
 def eth_cotract_state(contract_id):
-
     contract_creator = CreateContract("../files/MarriageContract.sol", template_settings=None)
 
     caller = contract_creator.get_contract_caller(contract_id)
@@ -96,22 +98,21 @@ def eth_cotract_state(contract_id):
 
     return jsonify(contract_values)
 
+
 @app.route('/api/eth_callback/<eng_key>')
 def eth_callback(eng_key):
-
     print("ETH callback ", eng_key)
 
-    user_a, user_b = str(r.get("engaged_"+eng_key).decode('ascii')).split("-")
+    user_a, user_b = str(r.get("engaged_" + eng_key).decode('ascii')).split("-")
     print("Broadcasting eth_callback to ", user_a, user_b)
 
     time.sleep(1)
-    broadcast(user_a,  'get_documents')
+    broadcast(user_a, 'get_documents')
     time.sleep(1)
     broadcast(user_b, 'get_documents')
     time.sleep(1)
 
     return jsonify({})
-
 
 
 @app.route('/api/generate_engaging_token', methods=['GET'])
@@ -136,6 +137,7 @@ def gencode():
     print("engaging_token response sent")
     return jsonify(response)
 
+
 def get_my_partner_name(my_id):
     print(f"Getting from redis partner id: partner_{my_id}")
     partner_id = r.get(f"partner_{my_id}")
@@ -150,8 +152,8 @@ def get_my_partner_name(my_id):
 def calculate_agreement(agr_a, agr_b):
     agr_c = {}
     binary_keys = [
-     'commitment_medial_records', 'commitment_hapiness', 'commitment_sexual_exclusivity',
-     'commitment_indefinite']
+        'commitment_medial_records', 'commitment_hapiness', 'commitment_sexual_exclusivity',
+        'commitment_indefinite']
 
     for k in binary_keys:
         if agr_a.get(k) == 'I do' and agr_b.get(k) == 'I do':
@@ -159,7 +161,7 @@ def calculate_agreement(agr_a, agr_b):
         else:
             agr_c[k] = "I don't"
 
-    a_duration = int(agr_a['commitment_duration']) if agr_a.get('commitment_duration','').isnumeric() else None
+    a_duration = int(agr_a['commitment_duration']) if agr_a.get('commitment_duration', '').isnumeric() else None
     b_duration = int(agr_b['commitment_duration']) if agr_b.get('commitment_duration', '').isnumeric() else None
 
     if a_duration or b_duration:
@@ -169,6 +171,7 @@ def calculate_agreement(agr_a, agr_b):
 
     print(agr_c)
     return agr_c
+
 
 @app.route('/api/load_aggrements_params', methods=['GET'])
 def load_aggrements_params():
@@ -181,7 +184,7 @@ def load_aggrements_params():
 
     params = {}
     keys = ['commitment_duration',
-            'commitment_medial_records','commitment_hapiness','commitment_sexual_exclusivity',
+            'commitment_medial_records', 'commitment_hapiness', 'commitment_sexual_exclusivity',
             'commitment_indefinite']
     for key in keys:
         if key in received:
@@ -218,8 +221,9 @@ def load_aggrements_params():
             "set_attributes": {}
         }
 
-    #store_to_blockchain(engagement_token, name_a=user_name, name_b=partner_name)
+    # store_to_blockchain(engagement_token, name_a=user_name, name_b=partner_name)
     return jsonify(params)
+
 
 @app.route('/static_file/<path:path>')
 def send_static(path):
@@ -234,10 +238,10 @@ def send_static(path):
 @app.route('/api/final_yes', methods=['GET'])
 def final_yes():
     received = request.args.to_dict(flat=False)
-    user_id = received['chatfuel user id'][0] # b'MSHM8W'
+    user_id = received['chatfuel user id'][0]  # b'MSHM8W'
     print(f"Received user_id {user_id}")
     engagement_token = str(r.get(f"gettoken_{user_id}"))[2:-1]
-    my_name  = f"{received['first name'][0]} {received['last name'][0]}"
+    my_name = f"{received['first name'][0]} {received['last name'][0]}"
     partner_name = get_my_partner_name(user_id)
     print("partner name:", partner_name)
     if not partner_name:
@@ -251,6 +255,7 @@ def final_yes():
                 "blockchain_recored_started": "true"
             }
     })
+
 
 @app.route('/api/validate_engaging_token', methods=['GET'])
 def validate_engaging_token():
@@ -272,11 +277,11 @@ def validate_engaging_token():
 
     if partner_id:
         response = {
-          "set_attributes":
-            {
-              "found_match": "true",
-              "partner_id": partner_id
-            }
+            "set_attributes":
+                {
+                    "found_match": "true",
+                    "partner_id": partner_id
+                }
         }
         r.set(f'engaged_{engaging_token}', f"{partner_id}-{user_id}")
         r.set(f'partner_{partner_id}', user_id)
@@ -300,6 +305,7 @@ def validate_engaging_token():
 
     return jsonify(response)
 
+
 @app.route('/api/get_documents', methods=['GET'])
 def get_documents():
     print("Received message", request.args)
@@ -307,7 +313,7 @@ def get_documents():
     user_id = received['chatfuel user id'][0]
     engaging_token = r.get(f"gettoken_{user_id}").decode('ascii')
     print(f"engtoken: {engaging_token}")
-    domain = 'plants.id' # 46.101.117.31
+    domain = 'plants.id'  # 46.101.117.31
     port = ""
 
     certificate_url = f"http://{domain}{port}/static_file/cert_{engaging_token}"
@@ -321,66 +327,67 @@ def get_documents():
     """)
 
     resp = {
-     "messages": [
-        {
-          "attachment":{
-            "type":"template",
-            "payload":{
-              "template_type":"generic",
-              "image_aspect_ratio": "square",
-              "elements":[
-                {
-                  "title":"Certificate",
-                  "image_url": f"{certificate_url}.png",
-                  "subtitle":"Your ETH certificate",
-                  "buttons":[
-                    {
-                      "type":"web_url",
-                      "url": f"{certificate_url}.png",
-                      "title":"View Item"
+        "messages": [
+            {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "image_aspect_ratio": "square",
+                        "elements": [
+                            {
+                                "title": "Certificate",
+                                "image_url": f"{certificate_url}.png",
+                                "subtitle": "Your ETH certificate",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": f"{certificate_url}.png",
+                                        "title": "View Item"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Power of attorney (A)",
+                                "image_url": f"{power_a_url}.png",
+                                "subtitle": "To print and sign up",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": f"{power_a_url}.png"
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": f"{power_a_url}.png",
+                                        "title": "View Item"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Power of attorney (B)",
+                                "image_url": f"{power_b_url}.png",
+                                "subtitle": "To print and sign up",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": f"{power_b_url}.png"
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": f"{power_b_url}.png",
+                                        "title": "View Item"
+                                    }
+                                ]
+                            }
+                        ]
                     }
-                  ]
-                },
-                    {
-                      "title":"Power of attorney (A)",
-                      "image_url": f"{power_a_url}.png",
-                      "subtitle": "To print and sign up",
-                      "default_action": {
-                        "type": "web_url",
-                        "url": f"{power_a_url}.png"
-                      },
-                      "buttons":[
-                        {
-                          "type":"web_url",
-                          "url":f"{power_a_url}.png",
-                          "title":"View Item"
-                        }
-                      ]
-                    },
-                    {
-                      "title": "Power of attorney (B)",
-                      "image_url": f"{power_b_url}.png",
-                      "subtitle": "To print and sign up",
-                      "default_action": {
-                          "type": "web_url",
-                          "url": f"{power_b_url}.png"
-                      },
-                      "buttons": [
-                          {
-                              "type": "web_url",
-                              "url": f"{power_b_url}.png",
-                              "title": "View Item"
-                          }
-                      ]
-                    }
-              ]
+                }
             }
-          }
-        }
-      ]
+        ]
     }
 
     return jsonify(resp)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
